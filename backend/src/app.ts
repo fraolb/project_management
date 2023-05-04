5cfc61cf10ba83ae2e7a1b99f6eec682b810fe4b
fraolb <Fraolbereket@gmail.com>
2023-05-04 11:49:42 +0300

    error handling and othe api's

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 
 import notesRoutes from "./routes/notes";
+import createHttpError, {isHttpError} from "http-errors";
 
 const app = express();
 
@@ -13,14 +14,18 @@ app.use(express.json());
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next)=>{
-    next(Error("End point not found"))
+    next(createHttpError(404,"End point not found"))
 })
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.log(error);
   let errorMessage = "Unknown error occured";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if(isHttpError(error)){
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
